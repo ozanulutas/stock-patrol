26f6fd480a6ce7051fae02fe500af08c8932aef0
ozanulutas <oznulutas@gmail.com>
2021-11-05 23:57:11 +0300

    feat: create snackbar

---
+++ src/components/BaseSnackbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-snackbar v-model="snackbar">
+    {{ text }}
+
+    <template v-slot:action="{ attrs }">
+      <v-btn
+        color="pink"
+        text
+        v-bind="attrs"
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'BaseSnackbar'
+};
+</script>
+
+<style>
+</style>
